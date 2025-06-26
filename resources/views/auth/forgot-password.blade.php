0e3672d9396b5455dc4a62bb08ad2b1df9123ebc
Dimasgnwn <gunawandimas82@gmail.com>
2025-06-26 19:25:02 +0700

    update component Auth

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,73 +1,13 @@
-<!doctype html>
-
-<html lang="en" class="layout-wide customizer-hide" dir="ltr" data-skin="default"
-    data-assets-path="{{ asset('portos/assets') }}" data-template="vertical-menu-template" data-bs-theme="light">
-
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport"
-        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-
-    <title>Lupa password|WARKOPOS</title>
-
-    <meta name="description" content="" />
-
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('portos/assets/img/favicon/favicon.ico') }}" />
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap"
-        rel="stylesheet" />
-
-    <link rel="stylesheet" href="{{ asset('portos/assets/vendor/fonts/iconify-icons.css') }}" />
-
-    <!-- Core CSS -->
-    <!-- build:css assets/vendor/css/theme.css  -->
-
-    <link rel="stylesheet" href="{{ asset('portos/assets/vendor/libs/node-waves/node-waves.css') }}" />
-
-    <link rel="stylesheet" href="{{ asset('portos/assets/vendor/libs/pickr/pickr-themes.css') }}" />
-
-    <link rel="stylesheet" href="{{ asset('portos/assets/vendor/css/core.css') }}" />
-    <link rel="stylesheet" href="{{ asset('portos/assets/css/demo.css') }}" />
-
-    <!-- Vendors CSS -->
-
-    <link rel="stylesheet" href="{{ asset('portos/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
-
-    <!-- endbuild -->
-
-    <!-- Vendor -->
-    <link rel="stylesheet" href="{{ asset('portos/assets/vendor/libs/@form-validation/form-validation.css') }}" />
-
-    <!-- Page CSS -->
-    <!-- Page -->
-    <link rel="stylesheet" href="{{ asset('portos/assets/vendor/css/pages/page-auth.css') }}" />
-
-    <!-- Helpers -->
-    <script src="{{ asset('portos/assets/vendor/js/helpers.js') }}"></script>
-    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
-    <script src="{{ asset('portos/assets/vendor/js/template-customizer.js') }}"></script>
-
-    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-
-    <script src="{{ asset('portos/assets/js/config.js') }}"></script>
-</head>
+<x-head-auth>Lupa Password</x-head-auth>
 
 <body>
     <!-- Content -->
 
-    @if (session('status'))
+    @if (session('success'))
         <script>
             Swal.fire({
                 title: 'Berhasil!',
-                text: '{{ session('status') }}',
+                text: '{{ session('success') }}',
                 icon: 'success',
                 confirmButtonText: 'OK'
             });
@@ -108,9 +48,9 @@
 
                     @if ($errors->any())
                         <div class="alert alert-danger">
-                            <ul>
+                            <ul class="d-flex flex-column justify-start align-content-center mb-1 p-0">
                                 @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
+                                    <li class="list-group-item ">{{ $error }}</li>
                                 @endforeach
                             </ul>
                         </div>
@@ -140,40 +80,15 @@
     <!-- / Content -->
 
     <!-- Core JS -->
-    <!-- build:js assets/vendor/js/theme.js -->
-
-    <script src="{{ asset('portos/assets/vendor/libs/jquery/jquery.js') }}"></script>
-
-
-    <script src="{{ asset('portos/assets/vendor/libs/popper/popper.js') }}"></script>
-    <script src="{{ asset('portos/assets/vendor/js/bootstrap.js') }}"></script>
-    <script src="{{ asset('portos/assets/vendor/libs/node-waves/node-waves.js') }}"></script>
-
-    <script src="{{ asset('portos/assets/vendor/libs/@algolia/autocomplete-js.js') }}"></script>
-
-    <script src="{{ asset('portos/assets/vendor/libs/pickr/pickr.js') }}"></script>
-
-    <script src="{{ asset('portos/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
-
-    <script src="{{ asset('portos/assets/vendor/libs/hammer/hammer.js') }}"></script>
-
-    <script src="{{ asset('portos/assets/vendor/libs/i18n/i18n.js') }}"></script>
-
-    <script src="{{ asset('portos/assets/vendor/js/menu.js') }}"></script>
-
-    <!-- endbuild -->
-
+    @extends('layouts.scripts')
+    @section('page-js')
     <!-- Vendors JS -->
     <script src="{{ asset('portos/assets/vendor/libs/@form-validation/popular.js') }}"></script>
     <script src="{{ asset('portos/assets/vendor/libs/@form-validation/bootstrap5.js') }}"></script>
     <script src="{{ asset('portos/assets/vendor/libs/@form-validation/auto-focus.js') }}"></script>
-
-    <!-- Main JS -->
-
-    <script src="{{ asset('portos/assets/js/main.js') }}"></script>
-
     <!-- Page JS -->
     <script src="{{ asset('portos/assets/js/pages-auth.js') }}"></script>
+    @endsection
 </body>
 
 </html>
