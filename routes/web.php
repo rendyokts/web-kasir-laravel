4498be82888827fc700e2a1065556da0e070ba00
Dimasgnwn <gunawandimas82@gmail.com>
2025-06-27 00:48:58 +0700

    p

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 // Bikin View nya dulu di Resources View
 // Bikin Controller di app/http/controllers pake `php artisan make:controller NamaFolder\NamaFileController` Nama Folder Awal Kapitar, Nama File AwalKapital + Controller
-// Bikin Routes nya di web.php 
+// Bikin Routes nya di web.php
 // Bikin Models di app\models pake `php artisan make:model NamaFolder\NamaFile`
 
 
@@ -20,5 +20,5 @@ include_once('farel.php');
 include_once('ilham.php');
 include_once('rakha.php');
 include_once('habib.php');
-include_once('rendy.php');
+include_once('rendi.php');
 include_once('dimas.php');
