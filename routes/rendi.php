73ac906df76275a7d7033fb014524443533269ae
rendy oktavian <rendyokts08@gmail.com>
2025-07-03 16:20:32 +0700

    commit rendy prefix routes

--- routes/rendi.php
+++ routes/rendi.php
@@ -1,10 +1,12 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\MasterUserController;
 
-//Master User
-Route::get('/master_user', [MasterUserController::class, 'index'])->name('master_user.index');
-Route::get('/add_user', [MasterUserController::class, 'createUser'])->name('master_user.add_user');
-Route::post('/save_user', [MasterUserController::class, 'saveUser'])->name('master_user.save');
-Route::get('/edit/{id}', [MasterUserController::class, 'editUser'])->name('master_user.edit');
-Route::delete('/delete/{id}', [MasterUserController::class, 'deleteUser'])->name('master_user.delete');
+Route::prefix('master_user')->group(function () {
+    Route::get('/', [MasterUserController::class, 'index'])->name('master_user.index');
+    Route::get('/add_user', [MasterUserController::class, 'createUser'])->name('master_user.add_user');
+    Route::post('/save_user', [MasterUserController::class, 'saveUser'])->name('master_user.save');
+    Route::get('/edit/{id}', [MasterUserController::class, 'editUser'])->name('master_user.edit');
+    Route::delete('/delete/{id}', [MasterUserController::class, 'deleteUser'])->name('master_user.delete');
+});
